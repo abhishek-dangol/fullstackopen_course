e54665dc93e1aea32d610267bb7124f78436b06b
abhishek-dangol <abhi.dangol@gmail.com>
2022-08-21 22:49:05 -0500

    Unicafe step1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import React from "react";
-import { useState } from 'react'
-
-
 
 const App = () => {
-  const [value, setValue] = useState(0)
-  const handleClick = () => {
-    setValue(value + 1)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClickGood = () => {
+    setGood(good + 1)
   }
+
+   const handleClickNeutral = () => {
+     setNeutral(neutral + 1);
+   };
   
+   const handleClickBad = () => {
+     setBad(bad + 1);
+   };
+
   return (
     <>
-      <p>{value}</p>
-      <button onClick={handleClick}></button>
+      <h2>give feedback</h2>
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    
     </>
-  );
+  )
 };
 
 export default App;
