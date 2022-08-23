322e26365594c95f5cd9bef8a472c14d122f0706
abhishek-dangol <abhi.dangol@gmail.com>
2022-08-22 22:56:33 -0500

    unicafe step2 step3 step4

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import { useState } from "react";
 import React from "react";
 
+const Statistics = (props) => {
+  if (!props.hasFeedback) {
+    return <p>No feedback has been given</p>
+  }
+  return (
+    <>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.good + props.bad + props.neutral) / props.total}</p>
+    </>
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [hasFeedback, setHasFeedback] = useState(false);
+
+  const total = good + bad + neutral;
 
   const handleClickGood = () => {
+    setHasFeedback(true);
     setGood(good + 1)
   }
 
-   const handleClickNeutral = () => {
+  const handleClickNeutral = () => {
+     setHasFeedback(true);
      setNeutral(neutral + 1);
    };
   
-   const handleClickBad = () => {
-     setBad(bad + 1);
+  const handleClickBad = () => {
+    setHasFeedback(true);
+     setBad(bad - 1);
    };
+  
+  const statisticsProps = {
+    hasFeedback: hasFeedback,
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+  };
 
   return (
     <>
-      <h2>give feedback</h2>
+      <h2>Give Feedback</h2>
       <button onClick={handleClickGood}>good</button>
       <button onClick={handleClickNeutral}>neutral</button>
       <button onClick={handleClickBad}>bad</button>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    
+      <Statistics {...statisticsProps} />
     </>
   )
 };
